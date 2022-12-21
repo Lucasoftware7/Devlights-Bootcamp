ae857f806abf55af127886445fd17d0a485623c5
Lucasoftware7 <94875496+Lucasoftware7@users.noreply.github.com>
2022-12-21 16:50:24 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hola mundo!");
+
+
+//class Ejercicios{
+
+/*   public void ejercicio_uno(string parameter)
+    {
+        
+    }*/
+//}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Devlights-Bootcamp
+ 
